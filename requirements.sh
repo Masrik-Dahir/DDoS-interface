00d7fedd12a0ff155728f6d5643ae567d54ef8e7
masri <dahirma@vcu.edu>
2022-04-05 02:47:58 -0400

    Interface ready!

--- requirements.sh
+++ requirements.sh
@@ -9,7 +9,6 @@ pip install PyQt5
 pip install pyqt5-tools
 pip install pybase64
 pip install scapy
-
 pip install cloudscraper
 pip install certifi
 pip intall dnspython
@@ -19,8 +18,6 @@ pip install psutil
 pip install icmplib
 pip install yarl
 pip install git+https://github.com/MHProDev/PyRoxy.git
-
-
-
-pyuic5 -x main.ui -o main.py
+pip install pyinstaller
+#pyuic5 -x main.ui -o main.py
 pyinstaller --onefile -w main.py
